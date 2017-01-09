0caf99a349b4f2a7def3d6520745cb94ab14b8b7
Raphael Barbo <rsbarbo@gmail.com>
2017-01-09 16:42:38 -0700

    update test to cover sad path

--- spec/features/user_can_edit_link_spec.rb
+++ spec/features/user_can_edit_link_spec.rb
@@ -18,5 +18,37 @@ RSpec.describe "User can edit created links", :js => :true do
     visit "links/#{Link.last.id}/edit"
 
     expect(page).to have_content("Edit Link Information")
+
+    fill_in "link-title", :with => "New Turing"
+    click_button "Save"
+
+    expect(current_path).to eq links_path
   end
+
+  scenario "after user creates a link, it cannot edit it with wrong params" do
+    fill_in "link-title", :with => "Turing"
+    fill_in "link-url", :with => "http://turing.io"
+    click_button "Add Link"
+
+    within('#links-list') do
+      expect(page).to have_text("Turing")
+      expect(page).to have_text("http://turing.io")
+    end
+
+    link = Link.last
+
+    visit "links/#{link.id}/edit"
+
+    expect(page).to have_content("Edit Link Information")
+
+    fill_in "link-url", :with => "turing.io"
+
+    click_button "Save"
+
+    save_and_open_page
+
+    expect(page).to have_content("Url is not a valid URL")
+    expect(current_path).to eq "/links/#{link.id}"
+  end
+
 end
