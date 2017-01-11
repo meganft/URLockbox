b2e80bd05f341fd2ee254bc93cd09a70b19fe66d
Raphael Barbo <rsbarbo@gmail.com>
2017-01-11 09:12:23 -0700

    add marks-read.js with fully functional functions and working properly (finally)

---
+++ app/assets/javascripts/marks-read.js
@@ -0,0 +1,67 @@
+var $newLinkTitle, $newLinkUrl;
+
+$(document).ready(function(){
+
+  function updateLinkOnRead(e) {
+    $('#link-' + this).addClass('read')
+
+    var button = $('#link-' + this).find('.mark-read');
+
+    button.removeClass('mark-read')
+    button.addClass('mark-unread')
+    button.html("Mark Unread")
+    button.parent().parent().find('.link_read').text("Read?: true")
+  };
+
+  function updateLinkOnUnread(e) {
+    $('#link-' + this).removeClass('read')
+
+    var button = $('#link-' + this).find('.mark-unread');
+
+    button.removeClass('mark-unread')
+    button.addClass('mark-read')
+    button.html("Mark as Read")
+    button.parent().parent().find('.link_read').text("Read?: false")
+  };
+
+  $('#links-list').on('click', 'button.mark-read', function(){
+
+    var $this = $(this);
+    var linkId = $this.parents('.link').data('id');
+
+    $.ajax({
+      url: '/api/v1/links/' + linkId,
+      method: 'PATCH',
+      data: {read: true}
+    }).then(updateLinkOnRead.bind(linkId));
+  });
+
+  $('#links-list').on('click', 'button.mark-unread', function(){
+
+    var $this = $(this);
+    var linkId = $this.parents('.link').data('id');
+
+    $.ajax({
+      url: '/api/v1/links/' + linkId,
+      method: 'PATCH',
+      data: {read: false}
+    }).then(updateLinkOnUnread.bind(linkId));
+  });
+
+
+  $("#links-list").children().each(function() {
+
+    var findStatus = $(this).find('.link_read').text().replace(/\s/g, '');
+    var readStatus = findStatus.split(":").pop();
+
+    if (readStatus == 'true') {
+      $(this).addClass('read')
+
+      var buttonSelector = $(this).find('.mark-read')
+
+      buttonSelector.removeClass('mark-read')
+      buttonSelector.addClass('mark-unread')
+      buttonSelector.html("Mark Unread")
+    }
+  });
+});
