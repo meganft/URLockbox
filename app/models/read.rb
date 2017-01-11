da3d8d84fc40090916c4d6660bccc9aa79bbce39
Raphael Barbo <rsbarbo@gmail.com>
2017-01-11 09:02:55 -0700

    break out hotreads service finally all clean out

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,3 +1,9 @@
+require "./app/services/hotreadsservice.rb"
+
 class Read < ApplicationRecord
   belongs_to :link
+
+  def self.create_hotreads(link)
+    HotReadsService.post_link(title: link.title, url: link.url)
+  end
 end
